40674714e9fb472e66e2f1639d6337873fe53532
Diego-Brandjes <diegobrandjes@hotmail.com>
2023-11-30 11:24:25 +0100

    Reformat of main for convention complience

--- arduino-uno.ino
+++ arduino-uno.ino
@@ -1,24 +1,40 @@
-#include "Arduino.h"
-#include <Wire.h>
+// Project Classes
 #include "HighTorqueServo.h"
 #include "Pump.h"
 #include "StateMachine.h"
 #include "Pump.h"
+
+// Standard libraries
+#include "Arduino.h"
+#include <Wire.h>
 #include <stdint.h>
 
-// Constants for Cup Holder Servo
+// Create an instance of the HighTorqueServo class for the Cup Holder
 const uint8_t CUP_HOLDER_SERVO_PIN = 6; // Pin number connected to the Cup Holder Servo
 const int CUP_HOLDER_MIN_ANGLE = 88;    // Minimum angle for the Cup Holder Servo
 const int CUP_HOLDER_MAX_ANGLE = 107;   // Maximum angle for the Cup Holder Servo
-// Create an instance of the HighTorqueServo class for the Cup Holder that initializes at 0.0 percent of the angle range
 HighTorqueServo cupHolderServo(CUP_HOLDER_SERVO_PIN, CUP_HOLDER_MIN_ANGLE, CUP_HOLDER_MAX_ANGLE);
 
-// Constants for the Pump
-const uint8_t PUMP_PIN = 7;
-
 // Create an instance of the Pump
+const uint8_t PUMP_PIN = 7;
 Pump pump(PUMP_PIN);
-StateMachine sm;
+
+// Create an instance of the state machine
+StateMachine stateMachine;
+
+// Function answer requests from the I2C connection
+void sendData() {
+  if (stateMachine.getState() == SM_IDLE_STATE) {
+    Wire.write(200); // TODO: Replace the placeholder data weight data
+  } else Wire.write(stateMachine.getState()); // Send the current state
+}
+
+// Function to receive state switches from the I2C connection
+void receiveData(int byteCount) {
+  while (Wire.available()) {
+    stateMachine.handleInputEvent(Wire.read());
+  }
+}
 
 void setup() {
   // Initialize the High Torque Servo component
@@ -28,22 +44,24 @@ void setup() {
   pump.init();
   cupHolderServo.init(0.0);
 
+  // Configure the I2C connection
   Wire.begin(0x8); // Arduino as a slave, with address 8
-  Wire.onReceive(receiveData); // Register a callback for incoming I2C data
   Wire.onRequest(sendData); // Register a callback for outgoing I2C data
-  Serial.begin(9600); // Sets baudrate for connection
+  Wire.onReceive(receiveData); // Register a callback for incoming I2C data
 }
 
 void loop() {
-  int state = sm.getState();
+  // Fetch the current state
+  int state = stateMachine.getState();
 
-  // DEBUG CODE FOR TESTING THE CUP HOLDER SERVO
-  if(sm.getState() == SM_TAPPING_STATE)
+  /* DEBUG CODE FOR TESTING THE I2C AND STATE MACHINE INTEGRATION
+  if(stateMachine.getState() == SM_TAPPING_STATE)
   {
     //if process has completed turn state to 2 as a flag t the RPi
-    sm.handleInputEvent(SM_ONE); // returns to the idle state
+    stateMachine.handleInputEvent(SM_ONE); // returns to the idle state
   }
   delay(3000);
+  */
 
   /*
     // DEBUG CODE FOR TESTING THE CUP HOLDER SERVO
@@ -60,18 +78,4 @@ void loop() {
     pump.stop();
     delay(1000);
   */
-}
-
-// Function to receive data on arrival
-void receiveData(int byteCount) {
-  while (Wire.available()) {
-    sm.handleInputEvent(Wire.read());
-  }
-}
-
-// Function to send data back over I2C 
-void sendData() {
-  if (sm.getState() == SM_IDLE_STATE) {
-    Wire.write(200); // temporary data, scale date here
-  } else Wire.write(sm.getState()); // Send the current state
 }
