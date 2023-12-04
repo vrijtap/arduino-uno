7e7c63bae02bd76778c8adb5a4d0ec25778c5aba
keladab <benjamin.kelada@hva.nl>
2023-12-04 14:30:53 +0100

    code

--- Scale.cpp
+++ Scale.cpp
@@ -1,19 +1,19 @@
 #include "Scale.h"
 
-HX711_Scale::HX711_Scale(int doutPin, int sckPin, float factor)
+Scale::Scale(int doutPin, int sckPin, float factor)
   : doutPin(doutPin), sckPin(sckPin), factor(factor) {
 }
 
-void HX711_Scale::init() {
+void Scale::init() {
   scale.init(doutPin, sckPin);
   scale.set_scale(factor);
   reset(); // reset the scale to 0
 }
 
-void HX711_Scale::reset() {
+void Scale::reset() {
   scale.tare();
 }
 
-float HX711_Scale::getWeight(int numOfReadings) {
+float Scale::getWeight(int numOfReadings) {
   return scale.get_units(numOfReadings);
 }
