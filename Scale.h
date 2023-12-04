8533ebceb03f7666268cc25fc7ee4832a2b73a3d
keladab <benjamin.kelada@hva.nl>
2023-12-04 14:40:35 +0100

    added comments header

--- Scale.h
+++ Scale.h
@@ -1,21 +1,28 @@
-#ifndef SCALE_H
+#ifndef SCALE_H  
 #define SCALE_H
 
-#include <Arduino.h>
-#include "HX711.h"
+#include <Arduino.h>  
+#include "HX711.h"    
 
 class Scale {
   public:
+    // Constructor: Initializes the Scale object with specified pins and calibration factor
     Scale(int doutPin, int sckPin, float factor);
+
+    // Initialize the scale
     void init();
+
+    // Reset the scale to zero
     void reset();
+
+    // Get the weight measurement after averaging 'numOfReadings' readings
     float getWeight(int numOfReadings);
 
   private:
-    int doutPin;
-    int sckPin;
-    float factor;
-    HX711 hx711;
+    int doutPin;    // Pin connected to the DOUT (data output) of the HX711
+    int sckPin;     // Pin connected to the SCK (serial clock) of the HX711
+    float factor;   // Calibration factor used to convert raw readings to weight units
+    HX711 hx711;    // Instance of the HX711 library for interfacing with the load cell
 };
 
-#endif
+#endif 
