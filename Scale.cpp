3063dc45206287fdc26f4387cd39cc89c63be5d4
keladab <benjamin.kelada@hva.nl>
2023-12-04 14:37:13 +0100

    changed variable names

--- Scale.cpp
+++ Scale.cpp
@@ -1,19 +1,19 @@
-#include "Scale.h"
+#include "HX711_Scale.h"
 
 Scale::Scale(int doutPin, int sckPin, float factor)
   : doutPin(doutPin), sckPin(sckPin), factor(factor) {
 }
 
 void Scale::init() {
-  scale.init(doutPin, sckPin);
-  scale.set_scale(factor);
+  hx711.begin(doutPin, sckPin);
+  hx711.set_scale(factor);
   reset(); // reset the scale to 0
 }
 
 void Scale::reset() {
-  scale.tare();
+  hx711.tare();
 }
 
 float Scale::getWeight(int numOfReadings) {
-  return scale.get_units(numOfReadings);
+  return hx711.get_units(numOfReadings);
 }
