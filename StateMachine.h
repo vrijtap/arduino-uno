bc1d2c54c134db2f32eed979b54d7d5368c8b46e
keladab <benjamin.kelada@hva.nl>
2023-12-04 14:49:41 +0100

    added right comments

--- StateMachine.h
+++ StateMachine.h
@@ -1,28 +1,32 @@
 // Made By: Benjamin Kelada
 
-#ifndef StateMachine_h
+#ifndef StateMachine_h  
 #define StateMachine_h
 
-#include <Arduino.h>
+#include <Arduino.h> 
 
 // Define states as constants
-#define SM_IDLE_STATE 0
-#define SM_TAPPING_STATE 1
-#define SM_PAUSED_STATE 2
+#define SM_IDLE_STATE 0   
+#define SM_TAPPING_STATE 1   
+#define SM_PAUSED_STATE 2   
 
 // Define events as constants
-#define SM_ZERO 0
-#define SM_ONE 1
+#define SM_ZERO 0   
+#define SM_ONE 1    
 
 class StateMachine {
 public:
+    // Constructor: Initializes the StateMachine object
     StateMachine();
+
+    // Function to get the current state
     int getState() const;
+
+    // Function to handle input events and change states
     void handleInputEvent(int SM_event);
-    
 
 private:
-    int SM_state;
+    int SM_state;   // Variable to hold the current state
 };
 
-#endif
+#endif  // End of header guard
