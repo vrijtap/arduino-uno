d6e4a46154d9b990c3c0282fc1fb253f8c83f279
brian-vdb <brianvdberg2107@gmail.com>
2023-12-14 11:54:52 +0100

    Bunch of shit

--- TappingSystem.cpp
+++ TappingSystem.cpp
@@ -1,4 +1,15 @@
 #include "TappingSystem.h"
 
 TappingSystem::TappingSystem(Scale *scale, HighTorqueServo *cupHolderServo, HighTorqueServo *armHolderServo, Pump *pump) : 
-  scale(scale), cupHolderServo(cupHolderServo), armHolderServo(armHolderServo), pump(pump) {}
+  scale(scale), cupHolderServo(cupHolderServo), armHolderServo(armHolderServo), pump(pump) {
+    this->tappingInfo.start = 0;
+    this->tappingInfo.amount = 0;
+}
+
+TappingSystem::update(void) {
+  return false;
+}
+
+TappingSystem::pause(void) {
+  return;
+}
--- TappingSystem.h
+++ TappingSystem.h
@@ -5,9 +5,26 @@
 #include "include/Pump.h"
 #include "include/Scale.h"
 
+#include <stdint.h>
+
+// Define a struct to hold tapping information
+struct TappingInfo {
+  int32_t start;
+  int16_t amount;
+};
+
 class TappingSystem {
 public:
+  // Constructor: Initializes the PumpSystem with the components to use
+  // scale: Scale object to measure weight
+  // cupHolderServo: Servo to tilt the cup
+  // armHolderServo: Servo to move the spout
+  // pump: Pump to tap the beer
   TappingSystem(Scale *scale, HighTorqueServo *cupHolderServo, HighTorqueServo *armHolderServo, Pump *pump);
+
+  bool update(void);
+
+  void pause(void);
 
 private:
   // Scale Object Pointer
@@ -19,6 +36,9 @@ private:
 
   // Pump Object Pointer
   Pump *pump;
+
+  // Struct to hold tapping information
+  TappingInfo tappingInfo;
 };
 
 #endif // TAPPING_SYSTEM_H
